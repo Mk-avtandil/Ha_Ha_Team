7db59bc3521638a572b2430ae86e302e69aca19b
avtandil <avtandilkgg@gmail.com>
2022-04-07 03:24:17 +0600

    Обновили код телеграм бота

--- telega.py
+++ telega.py
@@ -11,32 +11,50 @@ tg_bot = "5173934723:AAGg9SWw9CzKSRARzw9n1DN4gE4aCx3Be5s"
 bot = Bot(token=tg_bot)
 dp = Dispatcher(bot)
 
-cnt = -1
+cnt = 0
 f = open("news.json")
 data = json.load(f)
 
+next = KeyboardButton('Next')
+prev = KeyboardButton('Previous')
+markup = ReplyKeyboardMarkup(resize_keyboard=True).add(prev, next)
 
-myBtn = InlineKeyboardMarkup(row_width=1).add(
-    InlineKeyboardButton(text="Next", callback_data="Next")
-)
+def my_cnt(operator):
+    global cnt
+    if operator == '+':
+        cnt += 1
+    else:
+        if cnt >= 1:
+            cnt -= 1
+    return cnt
 
 def news():
-    global cnt
-    cnt += 1
     return str(data[cnt]['title']) + "\n" + \
         str("Фото: " + data[cnt]['photo']) + "\n" + \
         str("Cсылка: " + data[cnt]['link']) + "\n" + \
         str("Дата: " + data[cnt]['data'])
 
 
-@dp.callback_query_handler(text='Next')
-async def my_counter(callback: types.CallbackQuery):
-    await callback.message.answer(news())
-
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.answer(bold("Привет, пользователь 😎"), parse_mode=ParseMode.MARKDOWN)
-    await message.answer(news(), reply_markup=myBtn)
+    await message.reply(bold(
+        'Добро пожаловать, пользователь \n Я Kloopnews, бот созданный группой "Ha Ha Team 😎"'),
+        parse_mode=ParseMode.MARKDOWN)
+    await message.answer("Напишите команду /news и кайфуйте", reply_markup=markup)
+
+
+@dp.message_handler(commands=['news'])
+async def my_news(message: types.Message):
+    await message.answer(news(), reply=my_cnt('+'))
+
+
+@dp.message_handler(text='Next')
+async def btn_next(message: types.Message):
+    await message.answer(news(), reply=my_cnt('+'))
+
+@dp.message_handler(text='Previous')
+async def btn_next(message: types.Message):
+    await message.answer(news(), reply=my_cnt('-'))
 
 
 if __name__ == '__main__':
